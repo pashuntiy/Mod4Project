cfb9907cb27d195b0a7575b8672cac3445551ce1
EthanJF <ethanjosephfournier@gmail.com>
2019-12-02 17:47:32 -0500

    added a set interval feature

--- frontend/tamagotchi/src/App.js
+++ frontend/tamagotchi/src/App.js
@@ -3,15 +3,17 @@ import PetImage from './PetImage'
 import Stats from './Stats'
 import Actions from './Actions'
 import Mood from './Mood'
+import Timer from './Timer'
 
 export default class App extends Component {
 
   state = {
     hunger: 5,
-    social: 10,
-    fun: 10,
-    hygiene: 10,
-    mood: "sad"
+    social: 5,
+    fun: 5,
+    hygiene: 5,
+    mood: "sad",
+    timer: 0
   }
 
   increaseStats = (event) => {
@@ -21,16 +23,40 @@ export default class App extends Component {
     }, () => this.changeMood())
   }
 
+  decreaseStats = () => {
+    window.setInterval(() => {
+      const newHungerState = this.state.hunger === 0 ? 0 : this.state.hunger -1
+      const newSocialState = this.state.social === 0 ? 0 : this.state.social -1
+      const newFunState = this.state.fun === 0 ? 0 : this.state.fun -1
+      const newHygieneState = this.state.hygiene === 0 ? 0 : this.state.hygiene -1
+      
+      this.setState({
+        hunger: newHungerState,
+        social: newSocialState,
+        fun: newFunState,
+        hygiene: newHygieneState
+      }, () => this.changeMood())
+    }, 30000)
+  }
+
   changeMood = () => {
     if(this.state.hunger >= 10 && this.state.social >= 10 && this.state.fun >= 10 && this.state.hygiene >= 10){
       this.setState({
         mood: "happy"
+      })
+    } else if (this.state.hunger === 0 && this.state.social === 0 && this.state.fun === 0 && this.state.hygiene === 0){
+      this.setState({
+        mood: "dead"
       })
     } else {
       this.setState({
         mood: "sad"
       })
     }
+  }
+
+  componentDidMount(){
+    this.decreaseStats()
   }
 
   render(){
@@ -42,6 +68,7 @@ export default class App extends Component {
         <Stats hunger={this.state.hunger} social={this.state.social} fun={this.state.fun} hygiene={this.state.hygiene}/>
         <Actions hunger={this.state.hunger} social={this.state.social} fun={this.state.fun} hygiene={this.state.hygiene} increaseStats={this.increaseStats}/>
         <Mood mood={this.state.mood}/>
+        {/* <Timer handleClick={this.decreaseStats}/> */}
       </div>
     )
   }
--- frontend/tamagotchi/src/Mood.js
+++ frontend/tamagotchi/src/Mood.js
@@ -4,6 +4,8 @@ export default class Mood extends Component {
     renderImage = () => {
         if (this.props.mood === "happy") {
             return <img alt="smiley" src="http://clipart-library.com/images/6Tp5erXGc.jpg" />
+        } else if(this.props.mood === "dead") {
+            return <img alt="dead" src="http://clipart-library.com/images_k/shocked-emoji-transparent-background/shocked-emoji-transparent-background-20.png"/>
         } else {
             return <img alt="frowny" src="http://clipart-library.com/images/rinGgodBT.jpg" />
         }
