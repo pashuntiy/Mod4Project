b8ff4ca02c02911dced88ec3345330383e8663b3
EthanJF <ethanjosephfournier@gmail.com>
2019-12-04 17:09:51 -0500

    changes

--- backend/tamagotchi/app/serializers/user_serializer.rb
+++ backend/tamagotchi/app/serializers/user_serializer.rb
@@ -1,12 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
-  
   has_many :pets, through: :adopt_pets
-<<<<<<< HEAD
-=======
   has_many :adopt_pets
 
->>>>>>> ethan
 end
 
 
