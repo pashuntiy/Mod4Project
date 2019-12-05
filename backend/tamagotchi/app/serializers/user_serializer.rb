943b2b912047b425a2b88437dbd0f1c5f3dd698e
github username <github email address>
2019-12-05 09:33:21 -0500

    sieralizer

--- backend/tamagotchi/app/serializers/user_serializer.rb
+++ backend/tamagotchi/app/serializers/user_serializer.rb
@@ -1,14 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
-<<<<<<< HEAD
 
   has_many :pets, through: :adopt_pets
   has_many :adopt_pets
-=======
-  has_many :pets, through: :adopt_pets
-  has_many :adopt_pets
 
->>>>>>> 78db82b9739b7caa8b686368662870f74c0a6131
 end
 
 
