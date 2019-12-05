6718915e95fcd3473ce4a4cf3e288250f9b9db6b
EthanJF <ethanjosephfournier@gmail.com>
2019-12-05 12:34:34 -0500

    added feedme component

--- frontend/tamagotchi/src/components/Actions.js
+++ frontend/tamagotchi/src/components/Actions.js
@@ -1,71 +1,26 @@
 import React, { Component } from 'react'
 import TalkToMe from './TalkToMe'
+import FeedMe from './FeedMe'
 import ReactModal from 'react-modal'
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)'
-    }
-};
+
 
 
 export default class Actions extends Component {
 
-  state = {
-    showModal: false,
-    intervalID: null,
-    timer: 0
-  }
 
-    handleTimer = () => {
-        return window.setInterval(() => {
-            this.setState({
-                timer: this.state.timer + 1
-            }, console.log(this.state.timer))
-        }, 1000)
-    }
 
-    handleOpenModal = () => {
-        this.props.pauseGame()
-        this.setState({
-            showModal: true
-        })
-        const intervalId = this.handleTimer()
-        this.setState({
-            intervalId: intervalId
-        })
-    }
 
-    handleCloseModal = (event) => {
-        this.props.resumeGame()
-        this.setState({
-            showModal: false
-        })
-        window.clearInterval(this.state.intervalId)
-        this.props.increaseStats(event, this.state.timer)
-        this.setState({
-            timer: 0,
-            intervalID: null
-        })
-
-    }
 
     render(){
         return(
             <div className="actions">
                 <h1>Actions</h1>
                 <ul>
-                    <li><button onClick={(event) => this.props.increaseStats(event)}name="hunger">Feed Me</button></li>
-                    <li><button onClick={this.handleOpenModal}>Talk to Me</button></li>
-                    <ReactModal onKeyPress={this.handleKeyPress} isOpen={this.state.showModal} style={customStyles}>
-                        <TalkToMe/>
-                        <button name="social" onClick={(event) => this.handleCloseModal(event)}>End Conversation</button>
-                    </ReactModal>
+                   
+                    <li><FeedMe pauseGame={this.props.pauseGame} resumeGame={this.props.resumeGame} increaseStats={this.props.increaseStats} /></li>
+
+                    <li><TalkToMe pauseGame={this.props.pauseGame} resumeGame={this.props.resumeGame} increaseStats={this.props.increaseStats}/></li>
                     <li><button onClick={(event) => this.props.increaseStats(event)}name="fun">Play with Me</button></li>
                     <li><button onClick={(event) => this.props.increaseStats(event)}name="hygiene">Wash Me</button></li>
                 </ul>
---
+++ frontend/tamagotchi/src/components/FeedMe.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "../index.css";
+import ReactModal from 'react-modal'
+
+
+
+export default class FeedMe extends React.Component {
+
+    state = {
+        showModal: false,
+        intervalID: null,
+        active: false,
+        hotdog: false,
+        pancakes: false,
+        cupcake: false,
+        pizza: false,
+        points: 0
+    }
+
+    handleOpenModal = () => {
+        this.props.pauseGame()
+        this.setState({
+            showModal: true
+        })
+    }
+
+    handleCloseModal = (event) => {
+        this.props.resumeGame()
+        this.setState({
+            showModal: false
+        })
+        this.props.increaseStats(event, this.state.points)
+    }
+
+    handleClick = (event) => {
+        const stateName = event.target.name
+        const thesePoints = parseInt(event.target.dataset.points)
+        console.log(thesePoints)
+        this.setState({
+            [stateName]: !this.state[stateName],
+            points: this.state.points + thesePoints
+        }, console.log(this.state.points))
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleOpenModal}>Feed Me</button>
+                <ReactModal isOpen={this.state.showModal} className="hungerModal">
+                    <img alt="hotdog" name="hotdog" data-points="5" onClick={this.handleClick} src={require("../imgs/hotdog.gif")} />
+                    <img alt="pancakes" name="pancakes" onClick={this.handleClick} src={require("../imgs/pancakes.png")} />
+                    <img alt="cupcake" name="cupcake" onClick={this.handleClick} src={require("../imgs/cupcake.png")} />
+                    <img alt="pizza" name="pizza" onClick={this.handleClick} src={require("../imgs/pizza.png")} />
+                    <br />
+                    <h1>Selected Food:</h1>
+                    {this.state.hotdog ? (<img alt="hotdog" name="hotdog" onClick={(event) => this.handleClick(event)} src={require("../imgs/hotdog.gif")} />) : ""}
+                    {this.state.pancakes ? (<img alt="pancakes" name="pancakes" onClick={(event) => this.handleClick(event)} src={require("../imgs/pancakes.png")} />) : ""}
+                    {this.state.cupcake ? (<img alt="cupcake" name="cupcake" onClick={(event) => this.handleClick(event)} src={require("../imgs/cupcake.png")} />) : ""}
+                    {this.state.pizza ? (<img alt="pizza" name="pizza" onClick={(event) => this.handleClick(event)} src={require("../imgs/pizza.png")} />) : ""}
+
+                    <br />
+                    <button name="hunger" onClick={(event) => this.handleCloseModal(event)}>Give Pet Food</button>
+                </ReactModal>
+            </div>
+
+        )
+
+    }
+}
