fe3d333db499a5812b429cf100a98d226ccb5646
EthanJF <ethanjosephfournier@gmail.com>
2019-12-06 14:13:27 -0500

    just kidding, didn't fix anything

--- frontend/tamagotchi/src/components/PetCollection.js
+++ frontend/tamagotchi/src/components/PetCollection.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react'
 import PetCard from './PetCard'
-import { Route, Switch, Redirect } from 'react-router-dom'
 
 
 export default class PetCollection extends Component {
 
-    state = {
-        renderedPets: []
-    }
-
-    componentDidMount(){
-        if (this.props.pets !== undefined) {
-            this.setState({
-                renderedPets: this.props.pets.map((pet) => {
-                    return <PetCard pet={pet} handleClick={this.props.handleClick} key={pet.id} buttonText={this.props.buttonText} myPetCollection={this.props.myPetCollection} givePetUp={this.props.givePetUp} />
-                })
-            })
-        } 
-    }
-
     render(){
-       
+        const renderedPets= this.props.pets.map((pet) => {
+            return <PetCard pet={pet} handleClick={this.props.handleClick} key={pet.id} buttonText={this.props.buttonText} myPetCollection={this.props.myPetCollection} givePetUp={this.props.givePetUp} />
+        })
 
         return(
-
             <div className="pet-collection">
-                {console.log(this.state.renderedPets)}
-                {this.state.renderedPets.length === 0 ? (<Redirect to="/welcome" />) : this.state.renderedPets}
+                {renderedPets}
             </div>
         )
     }
 }
-
-
-
-// <Route path="/pet" render={ (props) => <PetCard {...props} pet={pet} handleClick={this.props.handleClick} key={pet.id} buttonText={this.props.buttonText} myPetCollection={this.props.myPetCollection} givePetUp={this.props.givePetUp}/> } />
